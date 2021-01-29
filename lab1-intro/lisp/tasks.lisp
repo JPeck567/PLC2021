4209fdc3bce7fb118d550a605290f69351adbe3e
Jorge <61948082+JPeck567@users.noreply.github.com>
2021-01-29 16:15:55 +0000

    practical 1 work

--- lab1-intro/lisp/tasks.lisp
+++ lab1-intro/lisp/tasks.lisp
@@ -1,13 +1,18 @@
 ;; code for Task 1.2(a):
-(print (1 + 2))
+(print (+ 1 2))
 ;; the above expression throws an error
+(terpri)
 
 ;; code for Task 1.2(b):
-(format t "type something: ")
+(format t "type something:  " )
 (setf input (read-line))
-(format t "you typed: ~a" input)
+(if (string= input "") 
+	(format t "you typed nothing dummy")
+	(format t "you typed: ~a" input))
 
 ;; code for Task 1.2(c):
 (setf prg '(+ 1 n)) ; define a very simple program
 (print prg) ; print the program
 ; TODO: execute the program with n = 1 and print its result
+(print ((eval prg) 1))
+dont know how to do it
